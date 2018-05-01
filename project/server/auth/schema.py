90e64d5588f37ce68ae55574d68e72f53ec9d0a5
dcDalin <mcdalinoluoch@gmail.com>
2018-05-02 02:32:03 +0300

    [Chore] :arrow_upper_right: Enhance validation using marshmallow-validators and wtforms

--- project/server/auth/schema.py
+++ project/server/auth/schema.py
@@ -2,17 +2,66 @@
 """Contains the schema for the auth endpoint"""
 
 from marshmallow import Schema, fields, post_load
+from marshmallow_validators.wtforms import from_wtforms
+from wtforms.validators import Length, Regexp
 from project.server.auth.models import User
+
+PASS_REG = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]"
 
 
 class UserSchema(Schema):
     """User schema"""
     user_id = fields.Int(dump_only=True)
-    user_level = fields.Int()
-    first_name = fields.Str(required=True)
-    last_name = fields.Str(required=True)
-    email = fields.Email(required=True)
-    password = fields.Str(required=True)
+    user_level = fields.Int(dump_only=True)
+    first_name = fields.Str(
+        required=True,
+        validate=from_wtforms(
+            [
+                Length(
+                    min=3,
+                    max=15,
+                    message="First name should be between 3 and 15 characters"
+                ),
+                Regexp(
+                    '^[^±!@£$%^&*_+§¡€#¢§¶•ªº«\\/<>?:;|=.,]{1,20}$',
+                    message="Invalid first name"
+                ),
+            ]
+        )
+    )
+    last_name = fields.Str(
+        required=True,
+        validate=from_wtforms(
+            [
+                Length(min=3, max=15,
+                       message="Last name should be between 3 and 15"),
+                Regexp(
+                    '^[^±!@£$%^&*_+§¡€#¢§¶•ªº«\\/<>?:;|=.,]{1,20}$',
+                    message="Invalid first name"
+                ),
+            ]
+        )
+    )
+    email = fields.Email(
+        required=True,
+        validate=from_wtforms(
+            [Length(min=8, max=50,
+                    message="Email should be between 8 and 50")]
+        )
+    )
+    password = fields.Str(
+        required=True,
+        validate=from_wtforms(
+            [
+                Length(min=8, max=20,
+                       message="Password should be between 8 and 20"),
+                Regexp(
+                    PASS_REG,
+                    message="Weak password"
+                ),
+            ]
+        )
+    )
     registered_on = fields.DateTime(dump_only=True)
 
     @post_load
